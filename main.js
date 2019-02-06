d98181c5ae54e1b71073a2187e94c99aa7297e97
Administrator <administrator@CN-1.lan>
2019-02-06 10:58:08 +0000

    upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const input = document.getElementsByTagName('html');
+
+
+function codeFind() {
+    let keyCode = event.code;
+    let doc = document.getElementById('eventCode');
+    doc.textContent = keyCode;
+};
+
+function whichFind() {
+    let keyWhich = event.which;
+    let doc2 = document.getElementsByClassName('eventWhich');
+    for (i = 0; i < doc2.length; i++){
+          doc2[i].textContent = keyWhich;  
+    }
+};
+
+function keyFind() {
+    let keyKey = event.key;
+    let doc1 = document.getElementById('eventKey');
+    doc1.textContent = keyKey;
+}
+
+document.addEventListener('keypress', () => {
+    keyFind();
+    whichFind();
+    codeFind();
+})
